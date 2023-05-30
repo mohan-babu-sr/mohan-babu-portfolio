16bf7de79af3c38f1dd7765426552a132c0b20b6
MOHAN BABU S R <writeto.mohanbabu@gmail.com>
2023-05-31 00:24:21 +0530

    basic setup
    header, dashboard & common component

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDividerModule } from '@angular/material/divider';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SpinnerComponentComponent } from './common/spinner-component/spinner-component.component';
+import { ButtonComponentComponent } from './common/button-component/button-component.component';
+import { MatDividerComponentComponent } from './common/mat-divider-component/mat-divider-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    SpinnerComponentComponent,
+    ButtonComponentComponent,
+    MatDividerComponentComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatDividerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
